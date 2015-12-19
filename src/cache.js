d175385d25be54a20645992d7a7cf166f4b45177
Oskar Börjesson <o.borjesson@gmail.com>
2015-12-19 18:51:03 +0100

    first test passing

--- src/cache.js
+++ src/cache.js
@@ -13,32 +13,44 @@ module.exports = class Cache {
     else {
       this.updates = new Rx.Subject();
     }
-    this._setReqSubject = new Rx.Subject();
+    this._setReqSubject = new Rx.Subject(),
     this._getReqSubject = new Rx.Subject();
-    this.asObservable = this.updates
+    this.asObservable =
+      this.updates
       .scan(
-        (state, operation) => operation(state),
+        (state, operation) => {
+          console.log(state, operation);
+          return operation(state);
+        },
         Im.Map())
-      .distinctUntilChanged().shareReplay(1)
+      .startWith(Im.Map())
+      .shareReplay(1);
   }
 
   get (requestedMap) {
-    let keys = Object.keys(requestedMap);
+    let keys = Im.fromJS(Object.keys(requestedMap).map(key => key.split('.')));
 
+    this.asObservable.take(1).subscribe((state) => {
+      let missingKeys = Im.List();
+      for (let i = 0; i < keys.size; i++) {
+        if (!state.hasIn(keys.get(i))) {
+          //TODO: modify in transaction
+          missingKeys = missingKeys.push(keys.get(i));
+        }
+      }
+      if (missingKeys.size > 0) {
+        this._getReqSubject.onNext(missingKeys);
+      }
+    });
     // find out if all keys are in the cache
     return this.asObservable.filter(
       (state) => {
-        let missingKeys = Im.List()
-        for (let key in keys.map(key => key.split('.'))) {
+        for (let key in keys) {
           if (!state.hasIn(key)) {
-            missingKeys.push(key);
+            return false; //Still haven't all keys in the cache
           }
         }
-        if (missingKeys.length > 0) {
-          this._getReqSubject.onNext(missingKeys);
-          return false;
-        }
-        else return true;
+        return true;
       }
     ).map((state) => {
       // This function only fires when all keys are in the cache (because of filter)
@@ -50,7 +62,7 @@ module.exports = class Cache {
             map.setIn(requestedMap[key].split('.'), cacheValue);
           });
         });
-      });
+      }).publish();
   }
 
   set (setMap) {
@@ -69,7 +81,14 @@ module.exports = class Cache {
   }
   get getRequests () {
     if(this.options.buffer) {
-      return this._getReqSubject.buffer(() => this._getReqSubject.debounce(this.options.buffer));
+      return this._getReqSubject
+      .buffer(() => this._getReqSubject.debounce(this.options.buffer))
+      .map(arr => {
+        return Im.Set().withMutations(
+          s => arr.map(i => s.union(i))
+        );
+      });
+      ;
     }
     else return this._getReqSubject;
   }
--- test/cache-test.js
+++ test/cache-test.js
@@ -2,12 +2,22 @@
 const test = require('tape'),
       Cache = require('../src/cache');
 
-test('Buffer: should buffer bursts', function (t) {
-  t.plan(1);
-  t.timeoutAfter(100)
+test('Cache: should buffer bursts of get requests', function (assert) {
+  assert.plan(1);
+  assert.timeoutAfter(100)
   var c = new Cache({buffer:1});
-  c.getRequests.subscribe((req) => t.equals({'a':'a', 'b': 'b'}))
-  c.get({'a':'a'});
-  c.get({'b': 'b'});
+  c.getRequests.subscribe((value) =>
+  {
+    assert.deepEquals(
+      value.toJS(), [['a'], ['b'], ['c']],
+      'requested keys "a", "b" and "c" are emitted at the same time'
+    );
+    assert.end();
+  });
+  c.get({'a':'localA'});
+  c.get({
+          'b': 'localB',
+          'c': 'localC'
+        });
 
 });
